17b213317573cf8ee4dbfdaba6136980faebfe64
Power366 <172065612+Power366@users.noreply.github.com>
2025-08-31 23:52:40 -0500

    tail + contains work

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ class LinkedList{
     constructor(){
         this.storage = {};
         this.first = 0;
+        this.counter=0;
+        this.index =0;
     }
     append(value){
         if(this.size()==0){
         this.first= value;
         this.storage[this.num()]= new Node(value)
         }else{
-            this.storage[this.tail()].next = value;
+            this.tail().next = value;
             this.storage[this.num()] = new Node(value)
         }
     }  
@@ -30,32 +32,49 @@ class LinkedList{
     }
     head(){
           for (const element of Object.keys(this.storage)) {
-            console.log(`${element} thats element key`);
             if(this.storage[element].value == this.first) return this.storage[element]
-            //if(this.storage.element.value == this.first) return this.storage.element;
+            
        }
     }
     tail(){
-       for (const element of this.storage) {
-            if(this.storage.element.next == 0) return element;
+       for (const element of Object.keys(this.storage)) {
+            if(this.storage[element].next == 0) return this.storage[element];
        }
     }
-    at(index){
+    traverse(node){
+            if(this.index == this.counter) return node
+            /*if(counter == 0){
+                counter++
+                traverse(this.head())
+            } */
+           this.counter = this.counter +1;
+            if(node.next == 0) return
+            let temp = node.next;
+            if(node.next != 0) this.traverse(this.storage[temp])
+            
 
+        }
+    at(index){
+        //let size = this.size()
+        this.index = index;
+        this.traverse(this.head());
     }
     pop(){
 
     }
     contains(value){
-
+          for (const element of Object.keys(this.storage)) {
+            if(this.storage[element].value == value) return true
+            
+       } return false
     }
     find(value){
 
     }
     toString(){
-        console.log(this.first);
-        console.log(this.storage["1"].value)
-       return this.storage;
+        let num = this.size();
+        let sum = this.head().value;
+       
     }
 }
 class Node{
@@ -65,7 +84,13 @@ class Node{
     }
 }
 let bob = new LinkedList();
-bob.preappend("Lion");
-console.log(bob.toString());
+bob.preappend("FirstElement");
+bob.append("lastElement");
+bob.append("NewLastElement");
+bob.preappend("NewFirstElement")
+//console.log(bob.toString());
 console.log(bob.head());
+console.log(bob.tail());
+console.log(bob.at(1))
+
 
